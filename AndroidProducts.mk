6967c2f9b5fa243a829975ba13d3b4df9ee34144
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-12 17:05:27 +0100

    a50: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A505F Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a50.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a50-user \
+    omni_a50-userdebug \
+    omni_a50-eng
